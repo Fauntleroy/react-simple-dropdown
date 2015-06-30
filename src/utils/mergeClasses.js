c510cee7066d2cd53508f015b4f9454be6c343cf
Timothy Kempf <the.kaempfer@gmail.com>
2015-06-29 18:25:48 -0700

    not actually using ES6 here

--- src/utils/mergeClasses.js
+++ src/utils/mergeClasses.js
@@ -1,6 +1,6 @@
 var mergeClasses = function(classes_object, class_string){
     var classes = class_string.split(' ');
-    for( let i = 0; i < classes.length; i++ ){
+    for( var i = 0; i < classes.length; i++ ){
         classes_object[classes[i]] = true;
     }
 };
