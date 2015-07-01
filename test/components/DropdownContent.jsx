517206106aabf1d5008eb6223fd657fc60f9274e
Timothy Kempf <the.kaempfer@gmail.com>
2015-07-01 09:42:42 -0700

    use jsx in test files

--- test/components/DropdownContent.jsx
+++ test/components/DropdownContent.jsx
@@ -8,7 +8,7 @@ var DropdownContent = require('../../components/DropdownContent.js');
 
 test( 'Merges classes from props with default element class', function( t ){
     t.plan( 2 );
-    var dropdown_content = TestUtils.renderIntoDocument( React.createElement( DropdownContent, { className: 'test' } ) );
+    var dropdown_content = TestUtils.renderIntoDocument( <DropdownContent className="test" /> );
     var dropdown_content = TestUtils.scryRenderedDOMComponentsWithClass( dropdown_content, 'dropdown__content' )[0];
     var dropdown_content_dom_node = React.findDOMNode( dropdown_content );
     t.ok( hasClass( dropdown_content_dom_node, 'dropdown__content' ), 'has class `dropdown__content`' );
--- test/components/DropdownTrigger.jsx
+++ test/components/DropdownTrigger.jsx
@@ -8,7 +8,7 @@ var DropdownTrigger = require('../../components/DropdownTrigger.js');
 
 test( 'Merges classes from props with default element class', function( t ){
     t.plan( 2 );
-    var dropdown_trigger = TestUtils.renderIntoDocument( React.createElement( DropdownTrigger, { className: 'test' } ) );
+    var dropdown_trigger = TestUtils.renderIntoDocument( <DropdownTrigger className="test" /> );
     var dropdown_trigger_element = TestUtils.findRenderedDOMComponentWithClass( dropdown_trigger, 'dropdown__trigger' );
     var dropdown_trigger_element_dom_node = React.findDOMNode( dropdown_trigger_element );
     t.ok( hasClass( dropdown_trigger_element_dom_node, 'dropdown__trigger' ), 'has class `dropdown__trigger`' );
@@ -18,7 +18,7 @@ test( 'Merges classes from props with default element class', function( t ){
 test( 'Attaches onClick handler to element', function( t ){
     t.plan( 1 );
     var clickHandler = function(){};
-    var dropdown_trigger = TestUtils.renderIntoDocument( React.createElement( DropdownTrigger, { onClick: clickHandler } ) );
+    var dropdown_trigger = TestUtils.renderIntoDocument( <DropdownTrigger onClick={clickHandler} /> );
     var dropdown_trigger_element = TestUtils.findRenderedDOMComponentWithClass( dropdown_trigger, 'dropdown__trigger' );
     t.equal( dropdown_trigger_element.props.onClick, clickHandler, 'clickHandler passed to DropdownTrigger and attached to anchor element are equal' );
 });
