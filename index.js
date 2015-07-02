4d8ba007141d278107010f2c08dbabdda80fda68
Timothy Kempf <the.kaempfer@gmail.com>
2015-07-02 13:43:10 -0700

    use react-simple-dropdown from npm

--- index.js
+++ index.js
@@ -128,13 +128,13 @@ process.umask = function() { return 0; };
 		return classes.substr(1);
 	}
 
-	if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = classNames;
+	} else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd){
 		// AMD. Register as an anonymous module.
 		define(function () {
 			return classNames;
 		});
-	} else if (typeof module !== 'undefined' && module.exports) {
-		module.exports = classNames;
 	} else {
 		window.classNames = classNames;
 	}
