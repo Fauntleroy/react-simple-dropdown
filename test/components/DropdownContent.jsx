ac81a6645c11cfb33da80033c9be75519219c06c
Timothy Kempf <the.kaempfer@gmail.com>
2015-07-14 16:26:20 -0700

    just use spaces

--- test/components/DropdownContent.jsx
+++ test/components/DropdownContent.jsx
@@ -7,12 +7,12 @@ var hasClass = domClasses.has;
 var DropdownContent = require('../../components/DropdownContent.js');
 
 var TestApp = React.createClass({
-	getInitialState: function(){
-	    return {};
-	},
-	render: function(){
-		return <DropdownContent className={this.state.className} />;
-	}
+    getInitialState: function(){
+        return {};
+    },
+    render: function(){
+        return <DropdownContent className={this.state.className} />;
+    }
 });
 var test_app = TestUtils.renderIntoDocument( <TestApp /> );
 var dropdown_content = TestUtils.findRenderedComponentWithType( test_app, DropdownContent );
@@ -23,11 +23,11 @@ test( 'Merges classes from props with default element class', function( t ){
     t.plan( 3 );
     t.equal( domClasses( dropdown_content_dom_node ).length, 1, 'has one class when `className` is empty' );
     test_app.setState({
-    	className: 'test'
+        className: 'test'
     });
     t.ok( hasClass( dropdown_content_dom_node, 'dropdown__content' ), 'has class `dropdown__content`' );
     t.ok( hasClass( dropdown_content_dom_node, 'test' ), 'has class `test`' );
     test_app.setState({
-    	className: null
+        className: null
     });
 });
